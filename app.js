e61160912e9479a1c4bd2ff7f987b11f440d1f76
D32SSV <131158288+D32SSV@users.noreply.github.com>
2025-10-30 14:42:54 +0530

    morgan logging + DB connection

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-// index.js
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.get('/', (req, res) => {
-  res.send(`Hello from Node backend running in Docker! ${PORT}`);
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("DB Connection Successfull !!!");
+  })
+  .catch((error) => {
+    console.error("!! Error connecting to MongoDB:", error);
+  });
+
+const allowedOrigins = ["http://localhost:5000"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `CORS error: origin not allowed`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: `Hello from Node backend running in ??Docker! ${PORT} in the docker container??`,
+  });
 });
 
 app.listen(PORT, () => {
