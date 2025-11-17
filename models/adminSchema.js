f6fc4efc4bf6887170e032954d0f1c1e5dba1fe4
D32SSV <131158288+D32SSV@users.noreply.github.com>
2025-11-17 23:13:44 +0530

    fix admin schema

--- models/adminSchema.js
+++ models/adminSchema.js
@@ -25,7 +25,7 @@ const AdminSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-Admin.pre("save", async function (next) {
+AdminSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   try {
     const salt = await bcrypt.genSalt(10);
