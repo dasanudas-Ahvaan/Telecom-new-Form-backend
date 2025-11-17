3b4cce982235116c5261bcd9bf55c02888740143
D32SSV <131158288+D32SSV@users.noreply.github.com>
2025-11-17 21:58:19 +0530

    rate limiter tested

--- middleware/otpLimiter.js
+++ middleware/otpLimiter.js
@@ -3,7 +3,10 @@ const rateLimit = require("express-rate-limit");
 const otpLimiter = rateLimit({
   windowMs: 5 * 60 * 1000,
   max: 1,
-  message: { message: "Too many OTP requests from this IP. Try later." },
+  message: {
+    success: false,
+    message: "Too many OTP requests. Try later.",
+  },
 });
 
 module.exports = { otpLimiter };
